87b8118a0bd0e6027d08a8b2f65719b400503fe8
LotusLou <loudavidquentin@gmail.com>
2025-01-28 21:33:07 +0100

    erstellt ein index.html file mit den programmpunkt en. Webdriver aktualisiert und templates hinzugefügt

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait ##from CO Pilot
-from selenium.webdriver.support import expected_conditions as EC ##from CO Pilot
-import time  
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 from datetime import datetime
+
 # Setup Driver
 service = Service(r"C:\Users\Lou\OneDrive - sluz\Maturaarbeit\Maturraarbeit\chromedriver.exe")
 driver = webdriver.Chrome(service=service)
@@ -14,7 +14,7 @@ driver = webdriver.Chrome(service=service)
 driver.get("https://www.schuur.ch/programm")
 time.sleep(5)  # Add a delay to allow the page to load completely
 wait = WebDriverWait(driver, 10)
-titleElements = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//meta[contains(@itemprop, 'performer')]"))) ##EC.presence_of_all_elements_located from CO Pilot
+titleElements = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//meta[contains(@itemprop, 'performer')]")))
 startdateElements = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//meta[contains(@itemprop, 'startDate')]")))
 enddateElements = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//meta[contains(@itemprop, 'endDate')]")))
 
@@ -24,11 +24,11 @@ print(f"Gefundene Enddaten: {len(enddateElements)}")
 
 # Prints the title and date of each program point
 for i in range(len(titleElements)):
-    title = titleElements[i].get_attribute("content") #verwende die range in i da man so mehrere listen die elemente herauslesen kann.
+    title = titleElements[i].get_attribute("content")
     startdate = startdateElements[i].get_attribute("content")
     enddate = enddateElements[i].get_attribute("content")
-    print(f"Programmpunkt {i+1}: {title} von {startdate} bis {enddate}") #f"" 
-
+    with open("index.html", "a", encoding="utf-8") as file:
+        file.write(f"<p>Programmpunkt {i+1}: {title} von {startdate} bis {enddate}</p>\n")
 
 # Close the driver
 driver.quit()
